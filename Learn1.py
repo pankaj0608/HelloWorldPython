adc307d01907bbdb748203aa317608c0715cb3a0
rsoni123 <Amaya123>
2018-06-05 20:06:56 -0400

    HelloWorldPython

---
+++ Learn1.py
@@ -0,0 +1,28 @@
+from collections import *
+
+text = "Donald Trump’s leading economic adviser says the president wants " \
+       "to strike separate, bilateral trade deals with Canada and Mexico rather" \
+       " than continue renegotiating the North American Free Trade Agreement." \
+       "Larry Kudlow, the director of Trump’s National Economic Council, says the " \
+       "president indicated that preference to him when they spoke on Monday.Kudlow was " \
+       "speaking Tuesday on the Fox News morning program, Fox and Friends.He also says he" \
+       " relayed that message to a senior member of the Prime Minister " \
+       "Justin Trudeau’s office.The remarks are likely to create more economic " \
+       "uncertainty between Canada and the U.S. following Trump’s imposition " \
+       "of steel and aluminum tariffs that now affect imports from this country," \
+       " Mexico and Europe. Donalds pankaj pankaj pankaj pankaj pankaj pankaj pankaj"
+
+words = text.split();
+
+print(words);
+
+counter = Counter(words);
+
+top_five = counter.most_common(4)
+
+
+# Hello
+print(top_five);
+print(top_five.__getitem__(1));
+
+
