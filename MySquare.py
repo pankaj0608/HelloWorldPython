c6b5f03c4ac2074e704abdb23f5612d48e2b4aba
rsoni123 <Amaya123>
2018-06-06 17:55:49 -0400

    HelloWorldPython

--- MySquare.py
+++ MySquare.py
@@ -6,14 +6,26 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor("red")
     brad = turtle.Turtle()
-    brad.color("yellow")
-    distane=200
-    degrees=-90
+    brad.color("lightblue")
+    brad.pensize(2)
+    brad.speed(10)
+    distance = 200
+    degrees = -90
     counter = 0
-    while counter < 4 :
-        brad.forward(distane)
-        brad.right(degrees)
-        counter = counter +1
+    rotation = 0
+    while rotation < 360:
+        brad.right(-1 * rotation)
+        print(-1 * rotation)
+
+        while counter < 4:
+            brad.forward(distance)
+            brad.right(degrees)
+            counter = counter + 1
+
+        rotation = rotation + 1
+        counter = 0
+
     window.exitonclick()
+
 
 draw_square()
